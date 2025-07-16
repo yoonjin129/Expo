202c8a641edeee4373a929412f97f03204ac8872
yoonjin129 <kyj1011403@gmail.com>
2025-07-16 17:57:05 +0900

    아인이랑 합친 거 (메인)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import MainPage from "./pages/MainPage";
 import AIScoreEvaluationPage from "./pages/AIScoreEvaluationPage";
 import ExercisePage from "./pages/ExercisePage";
 import ExerciseChoice from "./pages/ExerciseChoice";
 import ExerciseCamera from "./pages/ExerciseCamera";
-import "./App.css"; // 전역 스타일 (필요하면 유지)
+import "./App.css"; // 전역 스타일
 
 function App() {
   return (
@@ -30,6 +31,11 @@ function App() {
               gap: "20px",
             }}
           >
+            <li>
+              <Link to="/" style={{ color: "white", textDecoration: "none" }}>
+                메인
+              </Link>
+            </li>
             <li>
               <Link
                 to="/score"
@@ -67,7 +73,7 @@ function App() {
 
         {/* 라우팅 설정 */}
         <Routes>
-          <Route path="/" element={<AIScoreEvaluationPage />} />
+          <Route path="/" element={<MainPage />} /> {/* 메인페이지 */}
           <Route path="/score" element={<AIScoreEvaluationPage />} />
           <Route path="/exercise" element={<ExercisePage />} />
           <Route path="/choice" element={<ExerciseChoice />} />
